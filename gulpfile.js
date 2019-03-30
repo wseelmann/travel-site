7768a63d9dbe6dcbbacc80ff0930d4d44c6db908
Werner Seelmann <wseelmann@pc-seelmann.de>
2019-03-30 13:32:51 +0100

    install gulp and add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+	console.log("Hi - you created a Gulp task.");
+});
+
+gulp.task('html', function () {
+	console.log("Imagine something useful being to your HTML here")
+});
