79a50b8cffc4003ce8a87269c5dc33ae33c49b76
Werner Seelmann <wseelmann@pc-seelmann.de>
2019-03-30 14:56:18 +0100

    add gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch');
 
 gulp.task('default', function () {
 	console.log("Hi - you created a Gulp task.");
 });
 
 gulp.task('html', function () {
-	console.log("Imagine something useful being to your HTML here")
+	console.log("Imagine something useful being to your HTML here.")
+});
+
+gulp.task('styles', function () {
+	console.log("Imagine Sass or PostCSS tasks running here.")
+});
+
+gulp.task('watch', function () {
+	watch('./app/index.html', function () {
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*css', function () {
+		gulp.start('styles');
+	});
+
 });
