1442e890cf21a7f36e30a90226f8c0144a175465
Werner Seelmann <wseelmann@pc-seelmann.de>
2019-04-06 21:01:05 +0200

    Add mixins and start mobile first

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
+mixins = require('postcss-mixins'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
@@ -7,7 +8,7 @@ cssImport = require('postcss-import');
 
 gulp.task('styles', function () {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
 		.on('error', function (errorInfo) {
 			console.log(errorInfo.toString());
 			this.emit('end');
